fec265b8e61e92c95234bd0934492077b8b304e5
Seda Aydemir <seda.aydemir7@gmail.com>
2024-10-01 22:16:14 +0300

    ViewBag

--- PortfolioProject/Controllers/StatisticsController.cs
+++ PortfolioProject/Controllers/StatisticsController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PortfolioProject.Models;
 
 namespace PortfolioProject.Controllers
 {
     public class StatisticsController : Controller
     {
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
         // GET: Statistics
         public ActionResult Index()
         {
+            ViewBag.totalMessageCount = context.Contact.Count();
+            ViewBag.messageIsReadTrueCount = context.Contact.Where(x => x.IsRead == true).Count();
+            ViewBag.messageIsReadFalseCount =context.Contact.Where(y=>y.IsRead == false).Count();
+            ViewBag.skillCount =context.Skill.Count();
+            ViewBag.skillRateSum =context.Skill.Sum(x=>x.Rate);
+            ViewBag.skillRateAvg = context.Skill.Average(x => x.Rate);
+
+            var maxRate=context.Skill.Max(x=>x.Rate);
+            ViewBag.maxRateSkillName = context.Skill.Where(x => x.Rate == maxRate).Select(y=>y.SkillName).FirstOrDefault();
+
+
+            ViewBag.getMessageCountBySubjectReferances = context.Contact.Where(x => x.Subject == "Referans").Count();
+
+            ViewBag.getMessageCountByEmailContainHAndIsReadTrue = context.Contact.Where(x=>x.IsRead == true && x.Email.Contains("h")).Count();
+
+            ViewBag.getSkillNameByRate90 = context.Skill.Where(x=>x.Rate == 90).Select( y => y.SkillName).FirstOrDefault();
+
             return View();
         }
     }
