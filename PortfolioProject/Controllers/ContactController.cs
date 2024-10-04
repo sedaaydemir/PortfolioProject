d7d2a07c2f48f19bada7fc18c60833fd974b021a
Seda Aydemir <seda.aydemir7@gmail.com>
2024-10-04 21:35:14 +0300

    ...

--- PortfolioProject/Controllers/ContactController.cs
+++ PortfolioProject/Controllers/ContactController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PortfolioProject.Models;
 
 namespace PortfolioProject.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: Contact
+        DbMyPortfolioNightEntities context= new DbMyPortfolioNightEntities();
         public PartialViewResult PartialContactSidebar()
         {
             return PartialView();
         }
+
+        public PartialViewResult PartialContactDetail()
+        {
+            ViewBag.address= context.Profile.Select(x=>x.Address).FirstOrDefault();
+            ViewBag.description= context.Profile.Select(x=>x.Description).FirstOrDefault();
+            ViewBag.phone= context.Profile.Select(x=>x.Phone).FirstOrDefault();
+            ViewBag.email= context.Profile.Select(x=>x.Email).FirstOrDefault();
+
+            return PartialView();
+        }
+
+        public PartialViewResult PartialContactLocation()
+        {
+
+            return PartialView();
+        }
+
     }
 }
